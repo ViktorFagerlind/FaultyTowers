db697cf208c909f67cab82117c16acaead4df69b
ViktorFagerlind <viktor_fagerlind@hotmail.com>
2016-10-16 14:26:30 +0200

    Added some cubes dropping down...

---
+++ Assets/Scripts/CreateBoxes.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateBoxes : MonoBehaviour
+{
+  public Transform m_createObj;
+
+  // Use this for initialization
+  void Start ()
+  {
+	
+  }
+	
+
+  void Update()
+  {
+    if (Input.GetButtonDown ("Fire1") || (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began))
+    {
+      Instantiate (m_createObj, new Vector3 (Random.Range (-2f, 2f), 5f, 0f), Quaternion.identity);
+    }
+
+  }
+}
---
+++ Assets/Scripts/System/SystemCommands.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SystemCommands : MonoBehaviour 
+{
+	// Update is called once per frame
+	void Update () 
+  {
+    if (Input.GetKey("escape"))
+        Application.Quit();
+    
+    if (Input.GetKey("p"))
+      Application.CaptureScreenshot("Screenshot.png");
+	}
+}
