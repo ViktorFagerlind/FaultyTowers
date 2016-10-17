6ffff9b936116ea06963911e0acaf09e761943f8
Viktor Fägerlind <viktor_fagerlind@hotmail.com>
2016-10-17 23:35:41 +0200

    Added moving for mobile devices

--- Assets/Scripts/Blocks/MoveObject.cs
+++ Assets/Scripts/Blocks/MoveObject.cs
@@ -5,30 +5,97 @@ public class MoveObject : MonoBehaviour
 {
   private Vector3     m_downOffset;
   private Rigidbody2D m_rigidBody;
+  private bool        m_ongoingTouch = false;
+
+  // ------------------------------------------------------------------------------------------
 
   void Start ()
   {
-    m_rigidBody = this.GetComponents<Rigidbody2D>()[0];
+    m_rigidBody = this.GetComponents<Rigidbody2D> () [0];
   }
 
-  void OnMouseDown ()
+  // ------------------------------------------------------------------------------------------
+
+  void Update ()
   {
-    m_downOffset = transform.position - Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
+    if (Input.touchCount > 0)
+    {
+      switch (Input.GetTouch (0).phase)
+      {
+        case TouchPhase.Began:
+          Ray ray = Camera.main.ScreenPointToRay (Input.GetTouch (0).position);
+          if (Physics.Raycast (ray))
+          {
+            HandleDown (Input.GetTouch (0).position);
+          }
+          break;
 
-    Cursor.visible = false;
-    m_rigidBody.isKinematic = true;
+        case TouchPhase.Moved:
+          HandleMove (Input.GetTouch (0).position);
+          break;
+
+        case TouchPhase.Canceled:
+        case TouchPhase.Ended:
+          HandleUp ();
+          break;
+
+      }
+    }
   }
 
-  void OnMouseDrag ()
-  { 
-    Vector3 currentPosition = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
+  // ------------------------------------------------------------------------------------------
+
+  void HandleDown (Vector2 screenPosition)
+  {
+    m_downOffset = transform.position - Camera.main.ScreenToWorldPoint (new Vector3 (screenPosition.x, screenPosition.y, -Camera.main.transform.position.z));
+
+    Cursor.visible            = false;
+    m_rigidBody.isKinematic   = true;
+    m_ongoingTouch            = true;
+  }
+
+  // ------------------------------------------------------------------------------------------
+
+  void HandleMove (Vector2 screenPosition)
+  {
+    if (!m_ongoingTouch)
+      return;
+
+    Vector3 currentPosition = Camera.main.ScreenToWorldPoint (new Vector3 (screenPosition.x, screenPosition.y, -Camera.main.transform.position.z));
 
     transform.position = currentPosition + m_downOffset;
   }
 
+  // ------------------------------------------------------------------------------------------
+
+  void HandleUp ()
+  {
+    if (!m_ongoingTouch)
+      return;
+
+    Cursor.visible            = true;
+    m_rigidBody.isKinematic   = false;
+    m_ongoingTouch            = false;
+  }
+
+  // ------------------------------------------------------------------------------------------
+
+  void OnMouseDown ()
+  {
+    HandleDown (new Vector2 (Input.mousePosition.x, Input.mousePosition.y));
+  }
+
+  // ------------------------------------------------------------------------------------------
+
+  void OnMouseDrag ()
+  { 
+    HandleMove (new Vector2 (Input.mousePosition.x, Input.mousePosition.y));
+  }
+
+  // ------------------------------------------------------------------------------------------
+
   void OnMouseUp ()
   {
-    Cursor.visible = true;
-    m_rigidBody.isKinematic = false;
+    HandleUp ();
   }
 }
